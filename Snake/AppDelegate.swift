a6d26a6587ea75f163933ce9454d324fdc87f336
Joe Holt <wynatoej@gmail.com>
2017-08-22 23:13:24 -0500

    Initial Commit

---
+++ Snake/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Snake
+//
+//  Created by Joe Holt on 8/22/17.
+//  Copyright © 2017 Joe Holt. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
