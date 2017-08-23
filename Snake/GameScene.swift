20e302d221e300df328a1af7a728b4f84a2af39c
Joe Holt <wynatoej@gmail.com>
2017-08-23 01:43:23 -0500

    Snake can now die

--- Snake/GameScene.swift
+++ Snake/GameScene.swift
@@ -15,7 +15,7 @@ class GameScene: SKScene {
     private let frameUpdate = 15        //How often the frame will update(x/60)
     private var mainView: SKView!       //Scenekit view
     private var snake: Snake!           //Snake
-    private var doAddFood: Bool = false //Tell wheather or not to add food to snake
+    private var shouldExtendSnake: Bool = false //Tell wheather or not to add food to snake
     private var currentFrame = 0        //Counts what frame in order to tell when to update
     private var currentDirection: CGPoint = CGPoint(x: 0, y: 0  ) //Directoin snake will go
     
@@ -34,12 +34,27 @@ class GameScene: SKScene {
             var newPoint = snake.headPoistion
             newPoint?.x += currentDirection.x
             newPoint?.y += currentDirection.y
-            //Remove last node
-            if doAddFood == false {
+            //Check for death
+            //Edges
+            let max = gridSize/2 + 2
+            if Int(snake.headPoistion!.x) > max || Int(snake.headPoistion!.x) < -max || Int(snake.headPoistion!.y) > max || Int(snake.headPoistion!.y) < -max {
+                //Snake out of bounds
+                reset()
+                return
+            }
+            //Run into self
+            for point in snake.tail {
+                if point == newPoint && currentDirection != CGPoint(x: 0, y: 0) {
+                    reset()
+                    break
+                }
+            }
+            //Remove last node and check if snake should be made bigger
+            if shouldExtendSnake == false {
                 removeSquare(x: Int((snake.tail.last?.x)!), y: Int((snake.tail.last?.y)!))
                 snake.tail.removeLast()
             } else {
-                doAddFood = false
+                shouldExtendSnake = false
             }
             //Draw new square at new location
             snake.headPoistion = newPoint
@@ -72,6 +87,17 @@ class GameScene: SKScene {
         }
     }
     
+    private func reset() {
+        currentFrame = 0
+        snake = Snake(initialPoint: CGPoint(x: 0, y: 0))
+        currentDirection = CGPoint(x: 0, y: 0)
+        for child in children {
+            child.removeFromParent()
+        }
+        shouldExtendSnake = false
+        spawnFood()
+    }
+    
     private func spawnFood() {
         let randX = Int(arc4random_uniform(UInt32(gridSize))) - Int(gridSize/2)
         let randY = Int(arc4random_uniform(UInt32(gridSize))) - Int(gridSize/2)
@@ -79,7 +105,7 @@ class GameScene: SKScene {
     }
     
     private func didEatFood() {
-        doAddFood = true
+        shouldExtendSnake = true
     }
     
     private func drawSquare(x: Int, y: Int, color: SKColor = SKColor.white) {
@@ -88,7 +114,7 @@ class GameScene: SKScene {
         let yDim = (mainView.frame.size.height/CGFloat(gridSize)) * CGFloat(y)
         let square = SKSpriteNode()
         square.position = CGPoint(x: xDim, y: yDim)
-        square.size = CGSize(width: height, height: height - 2)
+        square.size = CGSize(width: height - 2, height: height - 2)
         square.color = color
         addChild(square)
     }
